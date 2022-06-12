d50a137c7e0d493d3db9bf0a3a0e20dd958743f8
saurabh <studsaurabh17@gmail.com>
2022-06-12 18:02:11 +0530

    new changes

--- server.js
+++ server.js
@@ -4,14 +4,14 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const cors=require('cors');
 require('dotenv').config();
-const port=process.env.PORT;
+const port=process.env.PORT || 5000;
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json())
 app.use(cors())
 //databse configurations
-const url = process.env.DATABASE_URL;
+const url = process.env.DATABASE_URL || `mongodb+srv://HEALTHBOOK:HEALTHBOOK@cluster.cirkd.mongodb.net/todo?retryWrites=true&w=majority`;
 
 mongoose.Promise = global.Promise;
 
